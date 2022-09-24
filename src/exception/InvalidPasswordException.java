7a16454b1611444d6bd3b8f8991e0d7446ca327e
MalaDoddamane <114280940+MalaDoddamane@users.noreply.github.com>
2022-09-24 13:02:20 +0530

    Adding Exception class

---
+++ src/exception/InvalidPasswordException.java
@@ -0,0 +1,32 @@
+package exception;
+
+public class InvalidPasswordException extends Exception {
+
+    int passwordConditionCount = 0;
+
+    public InvalidPasswordException(int passwordConditionCount) {
+        this.passwordConditionCount = passwordConditionCount;
+    }
+
+    public String printMessage() {
+        switch (passwordConditionCount) {
+
+            case 1:
+                return ("Password length should be more than 8 characters");
+
+            case 2:
+                return ("Password should not ne null");
+
+            case 3:
+                return ("Password should contain at least one uppercase letter(A-Z)");
+
+            case 4:
+                return ("Password should contain at least one lowercase letter(a-z)");
+
+            case 5:
+                return ("Password should contain at least one digit(0-9)");
+
+        }
+        return ("");
+    }
+}
